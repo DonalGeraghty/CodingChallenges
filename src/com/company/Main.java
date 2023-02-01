3fbb868e6d9184912f79a04ab228cded1f583eec
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2023-02-01 22:55:19 +0000

    adding sum of odd numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import com.company.kata.NumberFun;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        NumberFun numberFun = new NumberFun();
+        numberFun.findNextSquare(120);
+        numberFun.findNextSquare(121);
+        numberFun.findNextSquare(625);
+
     }
 }
