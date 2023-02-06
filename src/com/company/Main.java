0615f9a8758b89b2a8988470de62ed9b928df17e
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2023-02-06 23:22:46 +0000

    Removing test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NumberFun numberFun = new NumberFun();
-        numberFun.findNextSquare(120);
-        numberFun.findNextSquare(121);
-        numberFun.findNextSquare(625);
-
     }
 }
